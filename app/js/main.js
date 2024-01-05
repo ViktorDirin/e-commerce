b82ebcb7ea7167ade5831744fafa040eade09b7a
Viktor Dirin <vdirin@gmail.com>
2024-01-05 23:12:56 +0400

    added Clients section

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.clients__courusel').slick({
+        // dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+});
